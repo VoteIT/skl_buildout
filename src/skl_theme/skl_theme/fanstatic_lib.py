0a827136357847b205a4ae7355cdc76e7831fdf9
Robin Harms Oredsson <robin@betahaus.net>
2019-02-25 11:42:42 +0100

    Initial commit

---
+++ src/skl_theme/skl_theme/fanstatic_lib.py
@@ -0,0 +1,15 @@
+from fanstatic import Library
+from fanstatic import Resource
+from js.bootstrap import bootstrap_css
+from voteit.core.fanstaticlib import voteit_main_css
+
+
+library = Library('skl_theme', 'static')
+
+
+skl_custom_bootstrap_css = Resource(library, 'css/bootstrap.css', supersedes=(bootstrap_css,))
+skl_theme_css = Resource(library, 'css/main.css', depends = (skl_custom_bootstrap_css, voteit_main_css))
+
+
+def includeme(config):
+    pass
